83c9a091dafd09fbf2c5a276dfe29c66d5b9d4a1
willianbortolini <willian.borto@gmail.com>
2023-03-12 10:52:18 -0300

    Project created

---
+++ ExercicioEnum/ExercicioEnum/Entities/Enums/WorkerLever.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ExercicioEnum.Entities.Enums
+{
+    enum WorkerLever : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
---
+++ ExercicioEnum/ExercicioEnum/Entities/Worker.cs
@@ -0,0 +1,49 @@
+﻿using ExercicioEnum.Entities.Enums
+using System.Collections.Generic;
+
+namespace ExercicioEnum.Entities
+{
+    internal class Worker
+    {
+        public String Name { get; set; }
+        public WorkerLever Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Departament Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>;
+        
+        public Worker()
+        {
+
+        }
+
+        public Worker(string name, WorkerLever level, double baseSalary, Departament department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach(HourContract contract in Contracts)
+            {
+                if(contract.Date.Year ==  year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+        }
+    }
+}
