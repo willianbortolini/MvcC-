82d8dbea8ffc38e70dd9cb151b83b068e9a2e330
willianbortolini <willian.borto@gmail.com>
2023-03-19 20:24:20 -0300

    SellersController

---
+++ salesWebMvc4/salesWebMvc4/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace salesWebMvc4.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
