5581654ba4b145604010d98f252dbd7b1e3ce046
willianbortolini <willian.borto@gmail.com>
2023-03-21 20:02:57 -0300

    Simple Create form

--- salesWebMvc4/salesWebMvc4/Controllers/SellersController.cs
+++ salesWebMvc4/salesWebMvc4/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using salesWebMvc4.Models;
 using salesWebMvc4.Services;
 
 namespace salesWebMvc4.Controllers
@@ -16,6 +17,19 @@ namespace salesWebMvc4.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- salesWebMvc4/salesWebMvc4/Services/SellerService.cs
+++ salesWebMvc4/salesWebMvc4/Services/SellerService.cs
@@ -18,6 +18,12 @@ namespace salesWebMvc4.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);  
+            _context.SaveChanges();
+        }
     }
 
 }
