554f676f8c6b539d8ee27dfddc721380333179b7
willianbortolini <willian.borto@gmail.com>
2023-03-25 16:51:15 -0300

    Validation

--- salesWebMvc4/salesWebMvc4/Models/Seller.cs
+++ salesWebMvc4/salesWebMvc4/Models/Seller.cs
@@ -8,14 +8,24 @@ namespace salesWebMvc4.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="{0} required")]
+        [StringLength(60, MinimumLength = 3 , ErrorMessage ="{0} size shold be between {2} and {1}")]
         public string Name { get; set; }
-        [DataType(DataType.EmailAddress)]
+
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
+        [Required(ErrorMessage = "{0} required")]
+        [DataType(DataType.EmailAddress)]        
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birt Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
