6959eec32963f2f274e44001d94dc355c8bb41a8
willianbortolini <willian.borto@gmail.com>
2023-03-25 17:55:21 -0300

    Preparing sales search navigation views

---
+++ salesWebMvc4/salesWebMvc4/Controllers/SalesRecordsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace salesWebMvc4.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
